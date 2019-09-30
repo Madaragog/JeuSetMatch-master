e597b2b2cab81a3025f42855722f4719da72a2a9
Madaragog <cedric.dorez@gmail.com>
2019-09-30 09:35:25 +0200

    Second target for tests created

---
+++ JeuSetMatchTests/GameTestCase.swift
@@ -0,0 +1,15 @@
+//
+//  GameTestCase.swift
+//  JeuSetMatchTests
+//
+//  Created by Cedric on 30/09/2019.
+//  Copyright © 2019 OpenClassrooms. All rights reserved.
+//
+
+import XCTest
+
+class GameTestCase: XCTestCase {
+    func testExemple() {
+        XCTAssert(false)
+    }
+}
